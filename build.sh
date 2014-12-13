07d3f93d7265bba56cb1ac57e71182cf97b49437
eman <e@m.an>
2014-12-13 12:54:33 -0700

    set -e

--- build.sh
+++ build.sh
@@ -1,11 +1,15 @@
 #!/bin/bash
 
+set -e
+
 curl -O https://static.rust-lang.org/dist/rust-nightly.tar.gz
 tar -xf rust-nightly.tar.gz
 
 cd rust-nightly
+
 mkdir build
 cd build
+
 ../configure --target=arm-linux-gnueabihf
 make
 
