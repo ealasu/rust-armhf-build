4c450dad4203fddfc3cfc8f8d18737c8fa94ab1f
eman <e@m.an>
2014-12-15 01:02:52 +0000

    fix

--- build.sh
+++ build.sh
@@ -5,11 +5,11 @@ set -e
 curl -O https://static.rust-lang.org/dist/rust-nightly.tar.gz
 tar -xf rust-nightly.tar.gz
 
-mkdir rust-nightly-build
+mkdir -p rust-nightly-build
 cd rust-nightly-build
 
 ../rust-nightly/configure --target=arm-unknown-linux-gnueabihf
-make
+make -j8
 
 cd ..
 
