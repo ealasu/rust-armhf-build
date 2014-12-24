8f80664d101345a3a36232749e87bca3e8049131
eman <e@m.an>
2014-12-24 01:33:21 +0000

    fixes

--- scripts/build_source_package.sh
+++ scripts/build_source_package.sh
@@ -1,20 +1,20 @@
 #!/bin/bash
 set -e
 
-git submodule init
-git submodule update --depth 1
+rm -rf rust
+git clone https://github.com/rust-lang/rust --depth=1
 
 date="$(date --date="$(cd rust && git show -s --format=%ci)" +%Y%m%d%H%M)"
 hash="$(cd rust && git show -s --format=%h)"
 echo "last commit: $date $hash"
-version="$date~$hash~0ubuntu1"
+version="$date~$hash~utopic"
 echo "version: $version"
 
-debchange --newversion "$version" --release 'Changes.'
+tar -czf rust-nightly-armhf_${version}.orig.tar.gz rust
 
-rm -rf rust/debian
-tar -cf rust-nightly-armhf_${version}.orig.tar.gz rust
-
+export DEBEMAIL='Emanuel Alasu <emanuel.alasu@gmail.com>'
+debchange --newversion "$version" 'Changes.'
+debchange --release 'Releasing.'
 cp -r debian rust/
 
 (cd rust && debuild -S)
