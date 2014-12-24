7256808291758fe8e120424f1e4bf3f7a8bfb853
eman <e@m.an>
2014-12-24 00:54:22 +0000

    c

--- scripts/build_source_package.sh
+++ scripts/build_source_package.sh
@@ -1,10 +1,21 @@
 #!/bin/bash
-
 set -e
 
 git submodule init
 git submodule update --depth 1
 
-tar -cf rust-source.tar rust
-tar --append -f rust-source.tar --transform "s/^\./rust/" ./debian
+date="$(date --date="$(cd rust && git show -s --format=%ci)" +%Y%m%d%H%M)"
+hash="$(cd rust && git show -s --format=%h)"
+echo "last commit: $date $hash"
+version="$date~$hash~0ubuntu1"
+echo "version: $version"
+
+debchange --newversion "$version" --release 'Changes.'
+
+rm -rf rust/debian
+tar -cf rust-nightly-armhf_${version}.orig.tar.gz rust
+
+cp -r debian rust/
+
+(cd rust && debuild -S)
 
